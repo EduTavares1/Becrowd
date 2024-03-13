f155f87f7100d87b3c413aa0b67283316c4f77e4
EduTavares1 <157066609+EduTavares1@users.noreply.github.com>
2024-03-13 19:08:53 -0300

    Add files via upload

---
+++ Beecrowd/C99/1002.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  float raio,area,pi=3.14159;
+  
+  scanf("%f",&raio);
+  area= pi*pow(raio,2);
+  
+ 
+  
+  printf("A=%.4f",area);
+   
+
+
+    return 0;
+}
