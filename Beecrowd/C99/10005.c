90a32255627d90fae16eee1c5fefc0a62f155076
EduTavares1 <157066609+EduTavares1@users.noreply.github.com>
2024-03-13 22:00:40 -0300

    Update 10005.c

--- Beecrowd/C99/10005.c
+++ Beecrowd/C99/10005.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
- double A,B,MediaP;
+ double A,B,mediaP;
 
  scanf("%lf %lf",&A,&B);
 
- MEDIAP = (A*3.5) + (B*7.5)/11;
+ MEDIAP = ((A*3.5) + (B*7.5))/11;
 
- printf("MEDIA = %.5f",MEDIAP);
+ printf("MEDIA = %.5f",mediaP);
 
 
 
